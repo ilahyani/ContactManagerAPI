2acb8aea210cb795ed945257e07111d2d15a10b4
ilahyani <lahyani799@gmail.com>
2023-05-29 16:42:57 +0100

    added async funtionality to controllers and wrapped them in  asyncHandler to handle exceptions

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,47 +1,49 @@
+const asyncHandler = require("express-async-handler")
+
 //@desc GET ALL CONTACTS
 //@route GET /api/contacts
 //@access public
 
-const getContacts = (req, res) => {
-    res.json({msg: "GET CONTACTS"})
-}
+const getContacts = asyncHandler(async (req, res) => {
+    res.json({ msg: "GET CONTACTS" })
+})
 
 //@desc GET CONTACT
 //@route GET /api/contact/:id
 //@access public
 
-const getContact = (req, res) => {
-    res.json({msg: `GET CONTACT ${req.params.id}`})
-}
+const getContact = asyncHandler(async (req, res) => {
+    res.json({ msg: `GET CONTACT ${req.params.id}` })
+})
 
 //@desc CREATE CONTACT
 //@route GET /api/contacts
 //@access public
 
-const createContact = (req, res) => {
+const createContact = asyncHandler(async (req, res) => {
     console.log("request", req.body)
-    const {name, email, phone} = req.body
+    const { name, email, phone } = req.body
     if (!name || !email || !phone) {
         res.status(400)
         throw new Error("All fields are mandatory")
     }
-    res.json({msg: "CREATE CONTACT"})
-}
+    res.json({ msg: "CREATE CONTACT" })
+})
 
 //@desc EDIT CONTACT
 //@route GET /api/contact/:id
 //@access public
 
-const editContact = (req, res) => {
-    res.json({msg: `EDIT CONTACT ${req.params.id}`})
-}
+const editContact = asyncHandler(async (req, res) => {
+    res.json({ msg: `EDIT CONTACT ${req.params.id}` })
+})
 
 //@desc DELETE CONTACT
 //@route GET /api/contacts/:id
 //@access public
 
-const deleteContact = (req, res) => {
-    res.json({msg: `DELETE CONTACT ${req.params.id}`})
-}
+const deleteContact = asyncHandler(async (req, res) => {
+    res.json({ msg: `DELETE CONTACT ${req.params.id}` })
+})
 
-module.exports = {getContacts, getContact, createContact, editContact, deleteContact}
+module.exports = { getContacts, getContact, createContact, editContact, deleteContact }
