9066ace66430bd1dceb101b8d82f26e89946c0b8
ilahyani <lahyani799@gmail.com>
2023-05-30 14:07:56 +0100

    protecting contact routes so that only authorized users can perform CRUD operations

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -3,16 +3,16 @@ const contacts = require("../models/contactModel")
 
 //@desc GET ALL CONTACTS
 //@route GET /api/contacts
-//@access public
+//@access private
 
 const getContacts = asyncHandler(async (req, res) => {
-    const contactsTable = await contacts.find();
+    const contactsTable = await contacts.find({user_id: req.user.id});
     res.json(contactsTable)
 })
 
 //@desc GET CONTACT
 //@route GET /api/contact/:id
-//@access public
+//@access private
 
 const getContact = asyncHandler(async (req, res) => {
     try {
@@ -21,16 +21,20 @@ const getContact = asyncHandler(async (req, res) => {
             res.status(404)
             throw new Error("Contact does not exist")
         }
+        if (contact.user_id.toString() !== req.user.id) {
+            res.status(403)
+            throw new Error("Forbidden")
+        }
         res.json(contact)
     } catch (e) {
         res.status(404)
-        throw new Error("Contact does not exist")
+        throw new Error(e)
     }
 })
 
 //@desc CREATE CONTACT
 //@route GET /api/contacts
-//@access public
+//@access private
 
 const createContact = asyncHandler(async (req, res) => {
     const { name, email, phone } = req.body
@@ -38,13 +42,18 @@ const createContact = asyncHandler(async (req, res) => {
         res.status(400)
         throw new Error("All fields are mandatory")
     }
-    const newContact = await contacts.create(req.body)
+    const newContact = await contacts.create({
+        name: name,
+        email: email,
+        phone: phone,
+        user_id: req.user.id
+    })
     res.json(newContact)
 })
 
 //@desc EDIT CONTACT
 //@route GET /api/contact/:id
-//@access public
+//@access private
 
 const editContact = asyncHandler(async (req, res) => {
     try {
@@ -52,6 +61,10 @@ const editContact = asyncHandler(async (req, res) => {
         if (!contact) {
             res.status(404)
             throw new Error("Contact does not exist")
+        }
+        if (contact.user_id.toString() !== req.user.id) {
+            res.status(403)
+            throw new Error("Forbidden")
         }
         const updatedContact = await contacts.findByIdAndUpdate(
             req.params.id,
@@ -61,13 +74,13 @@ const editContact = asyncHandler(async (req, res) => {
         res.json(updatedContact)
     } catch (e) {
         res.status(404)
-        throw new Error("Contact does not exist")
+        throw new Error(e)
     }
 })
 
 //@desc DELETE CONTACT
 //@route GET /api/contacts/:id
-//@access public
+//@access private
 
 const deleteContact = asyncHandler(async (req, res) => {
     try {
@@ -76,11 +89,15 @@ const deleteContact = asyncHandler(async (req, res) => {
             res.status(404)
             throw new Error("Contact does not exist")
         }
-        const updatedContact = await contacts.findByIdAndDelete(req.params.id)
+        if (contact.user_id.toString() !== req.user.id) {
+            res.status(403)
+            throw new Error("Forbidden")
+        }
+        await contacts.deleteOne({_id: req.params.id})
         res.json("Contact Deleted")
     } catch (e) {
         res.status(404)
-        throw new Error("Contact does not exist")
+        throw new Error(e)
     }
 })
 
--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,10 +1,12 @@
 const router = require("express").Router()
+const validateTokenHandler = require("../middleware/validateTokenHandler")
 const { getContacts,
     getContact,
     createContact,
     editContact,
     deleteContact } = require("../controllers/contactController")
 
+router.use(validateTokenHandler)
 router.route("/").get(getContacts).post(createContact)
 router.route("/:id").get(getContact).put(editContact).delete(deleteContact)
 
